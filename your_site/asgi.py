38f6fbc29ae985f78910cf71032328b2fa83ac2a
Felix <hyperstown@gmail.com>
2020-12-02 14:11:10 +0100

    git ignore and names

--- your_site/asgi.py
+++ your_site/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for xmas_site project.
+ASGI config for your_site project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'xmas_site.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'your_site.settings')
 
 application = get_asgi_application()
--- your_site/wsgi.py
+++ your_site/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for xmas_site project.
+WSGI config for your_site project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'xmas_site.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'your_site.settings')
 
 application = get_wsgi_application()
