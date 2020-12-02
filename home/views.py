db2f2b978bc4fe0c1718f93d2bc758a722126dff
Felix <hyperstown@gmail.com>
2020-12-02 14:03:09 +0100

    inital code commit

---
+++ home/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from django.template import loader
+from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
+
+
+# class someView(LoginRequiredMixin, CreateView):
+#     login_url = "/login/"
+
+@login_required(login_url='/login/')
+def index(request):
+    template = loader.get_template('home/index.html')
+    context = {}
+    return HttpResponse(template.render(context, request))
